1b26e2626ff736fb11692f6477d6fb11079b073b
sri vardhan reddy <vardhanreddy2003@gmail.com>
2025-08-26 19:05:07 -0400

    Fix unsafe type usage and template literals in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,15 +1,20 @@
-// Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
+  const parsedNum = parseInt(num, 10);
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  if (isNaN(parsedNum)) {
+    res.status(400).send(`Invalid input: '${num}' is not a number`);
+    return;
+  }
+
+  const fibN: number = fibonacci(parsedNum) as number;
+  let result = `fibonacci(${parsedNum}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = `fibonacci(${parsedNum}) is undefined`;
   }
 
   res.send(result);
